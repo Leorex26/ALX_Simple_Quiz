3712420bc615e5c9212ae6a7197d78e6d7c26539
Effiong Ephraim <Effiongephraim95@gmail.com>
2025-03-03 08:58:49 -0800

    Initialize Recipe Sharing Application with Vite and Zustand

--- alx-fe-reactjs-main/my-company/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-      <footer style={{ textAlign: 'center', padding: '10px', background: '#ddd', marginTop: '20px' }}>
-        <p>© 2025 My Company. All rights reserved.</p>
-      </footer>
-    );
-  }
-  
-  export default Footer;  
